8a2c3a6ba70b6e13458ce208b6ec02555a9d2f0b
Jonathan Dinu <jdinu@berkeley.edu>
2013-01-22 15:28:03 -0800

    fixed rake file to work with the extra credit command

--- koans/path_to_enlightenment_extra.rb
+++ koans/path_to_enlightenment_extra.rb
@@ -1,28 +1,22 @@
-$LOAD_PATH << File.dirname(__FILE__) + "/extra"
+$LOAD_PATH << File.dirname(__FILE__)
 
-require 'about_array_assignment'
-
-require 'about_symbols'
-require 'about_regular_expressions'
-
-require 'about_constants'
-require 'about_triangle_project'
-require 'about_exceptions'
-require 'about_triangle_project_2'
-
-require 'about_sandwich_code'
-require 'about_scoring_project'
-
-require 'about_open_classes'
-require 'about_dice_project'
-require 'about_inheritance'
-require 'about_modules'
-
-require 'about_class_methods'
-require 'about_message_passing'
-require 'about_proxy_object_project'
+require 'extra/about_array_assignment'
+require 'extra/about_regular_expressions'
+require 'extra/about_constants'
+require 'extra/about_triangle_project'
+require 'extra/about_exceptions'
+require 'extra/about_triangle_project_2'
+require 'extra/about_sandwich_code'
+require 'extra/about_scoring_project'
+require 'extra/about_open_classes'
+require 'extra/about_dice_project'
+require 'extra/about_inheritance'
+require 'extra/about_modules'
+require 'extra/about_class_methods'
+require 'extra/about_message_passing'
+require 'extra/about_proxy_object_project'
 
 in_ruby_version("jruby") do
-  require 'about_java_interop'
+  require 'extra/about_java_interop'
 end
-require 'about_extra_credit'
+require 'extra/about_extra_credit'
