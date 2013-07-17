3a0207ae3ca20cef68e958d5a644b0dbea6a2497
Hack Reactor Students <github@hackreactor.com>
2013-07-16 21:50:53 -0700

    WIP adding methods to tree

--- ruby_data_structures/src/tree.rb
+++ ruby_data_structures/src/tree.rb
@@ -6,16 +6,35 @@ class Tree
     @children = []
   end
 
+  def contains value
+    bool = false
+    if @value = value
+      bool = true #yield?
+    else
+      #iterate through children and then see if
+      #children have children
+  end
+
   def create_tree value
     @value = value
     @children = []
   end
 
   def add_child value
-    @children.push(value)
+    new_child = Child.new(value)
+    @children.push(new_child)
   end
 
   def remove_child value
     @children.delete(value)
+  end
+end
+
+class Child
+  attr_accessor :value
+  attr_accessor :children
+  def initialize value
+    @value = value
+    @children = []
   end
 end
